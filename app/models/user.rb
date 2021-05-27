25b4c80f50bb9e4ae3375813e61d92228cd1f859
crappypapa <devsolaoluwa@gmail.com>
2021-05-27 14:55:27 +0100

    Add associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :groups, foreign_key: :author_id
+  has_many :contributions, foreign_key: :author_id, dependent: :destroy
+
+  validates :name, :email, presence: true, length: { minimum: 3, maximum: 25 }
+
 end
