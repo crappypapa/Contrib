a4a58816235b934d7e0daafbfcc3d7e31f555067
crappypapa <devsolaoluwa@gmail.com>
2021-05-27 15:16:12 +0100

    Update schema after running validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_113814) do
+ActiveRecord::Schema.define(version: 2021_05_27_135108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contribution_groups", force: :cascade do |t|
+    t.bigint "contribution_id", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["contribution_id"], name: "index_contribution_groups_on_contribution_id"
+    t.index ["group_id"], name: "index_contribution_groups_on_group_id"
+  end
+
+  create_table "contributions", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount"
+    t.bigint "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_contributions_on_author_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.integer "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -28,4 +53,7 @@ ActiveRecord::Schema.define(version: 2021_05_27_113814) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "contribution_groups", "contributions"
+  add_foreign_key "contribution_groups", "groups"
+  add_foreign_key "contributions", "users", column: "author_id"
 end
