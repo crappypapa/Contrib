1b5afafa315cd893d2223312ee18af87a533af22
crappypapa <devsolaoluwa@gmail.com>
2021-05-28 12:44:21 +0100

    Refactor User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,17 +15,13 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # GET /users/1/edit
-  def edit
-  end
-
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
+        format.html { redirect_to @user, notice: "Welcome to Contri #{@user.name}" }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
