465817fa990d4a79cfdb62739e98c12069df60bb
crappypapa <devsolaoluwa@gmail.com>
2021-05-28 12:42:45 +0100

    Update route resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
+  resources :groups
+  resources :contribution
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
