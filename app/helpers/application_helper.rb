623f09d6a5a1bd0f99bce4029d1d0adc25ecca2b
crappypapa <devsolaoluwa@gmail.com>
2021-05-31 08:50:26 +0100

    Add Session controllers and helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def app_nav
+    if current_user
+      "<div>
+        #{link_to current_user.name, user_path(current_user), class: 'btn'}
+        #{link_to 'Log out', logout_path, class: 'btn'}
+      </div>".to_s.html_safe
+    else
+      "<div>
+        #{link_to 'Sign In', new_session_path, class: 'btn'}
+        #{link_to 'Sign Up', new_user_path, class: 'btn'}
+      </div>".html_safe
+    end
+  end
 end
