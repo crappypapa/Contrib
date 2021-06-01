a4fa8b0792b19a82000f0ad3588f395b628148dc
crappypapa <devsolaoluwa@gmail.com>
2021-06-01 11:59:40 +0100

    Move Logic to helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,20 @@
 module ApplicationHelper
-  def app_nav
-    if current_user
-      "<div>
-        #{link_to current_user.name, user_path(current_user), class: 'btn'}
-        #{link_to 'Log out', logout_path, class: 'btn'}
-      </div>".to_s.html_safe
+  def app_nav(current_user)
+    if logged_in?
+      "<ul class = 'justify-content-end list-style-none col-md-8'>
+          #{link_to 'Contributions', contributions_path, class: 'nav-item btn btn-light mx-2'}
+          #{link_to 'Ungrouped Contributions', ungrouped_path, class: 'nav-item btn btn-light mx-2'}
+          #{link_to 'Contribution Groups', groups_path, class: 'nav-item btn btn-light mx-2'}
+          #{link_to 'Log Out', "/logout", class: 'navbar-right nav-item btn btn-danger mx-2 text-decoration-none'}
+          #{link_to current_user.username, "/#",class: 'nav-item  fa fa-user btn btn-primary mx-2 '}
+      </ul>".html_safe
     else
-      "<div>
-        #{link_to 'Sign In', new_session_path, class: 'btn'}
-        #{link_to 'Sign Up', new_user_path, class: 'btn'}
-      </div>".html_safe
+      "<ul class='d-flex justify-content-end list-style-none'>
+        #{link_to 'Sign Up', new_user_path, class: 'navbar-right nav-item btn btn-light mx-2 text-decoration-none'}
+        
+        #{link_to 'Log In', "/login", class: 'navbar-right nav-item btn btn-light text-decoration-none'}
+      </ul>".html_safe
     end
   end
+  
 end
