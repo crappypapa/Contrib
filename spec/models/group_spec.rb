0cf42f6ecae8689c1d15bb717d0b3a15a76db120
crappypapa <devsolaoluwa@gmail.com>
2021-06-01 08:58:16 +0100

    Removed commented code

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -11,26 +11,3 @@ RSpec.describe Group, type: :model do
     it { should validate_length_of(:name).is_at_least(3) }
   end
 end
-
-# RSpec.describe Group, type: :model do
-#   let(:user) { User.create(username: 'shaher', id: 1) }
-#   let(:group) { Group.create(name: 'shaher', author_id: user.id, id: 1) }
-
-#   it 'should be valid with valid attributes' do
-#     expect(group).to be_valid
-#   end
-
-#   it 'should be not valid without a username' do
-#     group.name = ''
-#     expect(group).to_not be_valid
-#   end
-
-#   it 'should be not valid without a user' do
-#     group.author_id = nil
-#     expect(group).to_not be_valid
-#   end
-
-#   it 'checks correct association between user and groups' do
-#     user.groups.should include(group)
-#   end
-# end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,7 @@
 require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   describe 'associations' do
-#     it { should have_many(:contributions).with_foreign_key(:author_id) }
-#     it { should have_many(:groups).with_foreign_key(:author_id) }
-#     it { should have_many(:contributions).dependent(:destroy) }
-#     it { should_not belong_to(:contribution_groups) }
-#   end
-#   describe 'validations' do
-#     it { should validate_presence_of(:username) }
-#     it { should validate_length_of(:username).is_at_least(3) }
-#     it { should validate_uniqueness_of(:username) }
-#   end
-# end
-
 RSpec.describe User, type: :model do
-  let(:user) { User.create(username: 'shaher') }
+  let(:user) { User.create(username: 'Olaoluwa') }
   let(:invalid_user) { User.create(username: '') }
   let(:contributions) { User.reflect_on_association(:contributions).macro }
   let(:groups) { User.reflect_on_association(:groups).macro }
